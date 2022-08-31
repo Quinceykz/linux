a55fa22a74c36579daf8c4f958a40ceeb9043f72
Quinceykz <kangzhenemail@163.com>
2022-08-31 13:35:12 +0800

    add a new proj 8.1.6(W/R)

---
+++ 8section/8_1_6_2/W_Proj/test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>   
+#include <sys/shm.h>
+
+#ifdef TEST
+
+#else
+
+
+int main()
+{
+        int shmid;
+        char *ptr;
+        char *shm_str = "string in a share memory";
+
+        shmid = shmget(0x90, 1024, SHM_W|SHM_R|IPC_CREAT|IPC_EXCL);
+
+        if (-1==shmid)
+                perror("create share memory");
+
+        ptr = (char*)shmat(shmid, 0, 0);
+        if ((void*)-1==ptr)
+                perror("get share memory");
+        
+        strcpy(ptr, shm_str);
+        shmdt(ptr);
+
+        return 0;
+
+}
+
+#endif
