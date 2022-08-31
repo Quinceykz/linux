135eaf78e8616bab3122d9e37a6311dee7f4a5fd
Quinceykz <kangzhenemail@163.com>
2022-08-31 15:16:02 +0800

    add a new proj of 8.2.2

---
+++ 8section/8_2_2/test.c
@@ -0,0 +1,31 @@
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void* thread_func(void *arg)
+{
+        int *val = arg;
+        printf("Hi, I'm a thread!\n");
+        if (NULL!=arg) {
+                while(1)
+                        printf("argument set: %d\n", *val);
+        }
+
+}
+               
+int main()
+{
+        pthread_t tid;
+        int t_arg = 100;
+
+        if (pthread_create(&tid, NULL, thread_func, &t_arg))
+                perror("Fail to create thread");
+
+        sleep(1);
+        pthread_cancel(tid);            //stop the pthread before the return of main
+        printf("Main thread!\n");
+
+        return 0;
+}
