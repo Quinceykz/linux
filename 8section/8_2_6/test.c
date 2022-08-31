85a2d7e28962a624bab050a6eb73b83f67737d65
Quinceykz <kangzhenemail@163.com>
2022-08-31 17:37:40 +0800

    add some explain of 8.1.6

--- 8section/8_2_6/test.c
+++ 8section/8_2_6/test.c
@@ -10,21 +10,23 @@ void* term_thread(void *arg);
 int main()
 {
         pthread_t mid_tid, term_tid;
-
+// int pthread_create (address of thread,  property of thread,  function,  
+// parameter of function);
         if (pthread_create(&mid_tid, NULL, mid_thread, NULL)) {
-
+// Provide the declarations for `sys_errlist' and `sys_nerr' if they
+// are available on this system.  void perror(const char *__s);
                 perror("Create mid thread error!");
                 return 0;
         }
 
         if (pthread_create(&term_tid, NULL, term_thread, &mid_tid)) {
-
+               
                 perror("Create mid thread fail!");
                 return 0; 
         }
 
         if (pthread_join(mid_tid, NULL)) {
-
+// int pthread_join (thread's name, thread'return);
                 perror("Wait mid thread error!");
                 return 0; 
         }
@@ -44,6 +46,7 @@ void* mid_thread(void *arg)
         printf("mid thread created!\n");
         while(2) {
                 printf("waitting term thread %d times!\n",times);
+// unsigned int sleep (seconds of main delay);
                 sleep(1);
                 times++;
         }
@@ -56,6 +59,7 @@ void* term_thread(void *arg)
         sleep(2);
         if (NULL!=arg) {
                 tid = arg;
+// stop the pthread after the end of thread
                 pthread_cancel(*tid);
         }
 }
